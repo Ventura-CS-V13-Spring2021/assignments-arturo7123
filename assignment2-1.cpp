3529e316267f09af3595cbe4203c62f9ee5f46ee
arturo7123 <chiefmerk73@yahoo.com>
2021-02-23 05:30:53 +0000

    wording

--- assignment2-1.cpp
+++ assignment2-1.cpp
@@ -8,15 +8,15 @@ int main()
      
      if (number1 > number2)
         if (number1 > number3)
-          cout << number1 << " is the greatest number!" ;
+          cout << number1 << " greatest number" ;
 
      if (number2 > number1)
         if (number2 > number3)
-          cout << number2 << " is the greatest number!" ;
+          cout << number2 << " greatest number" ;
 
      if (number3 > number1)
         if (number3 > number2)
-          cout << number3 << " is the greatest number!" ;
+          cout << number3 << " greatest number" ;
 
      return 0;
 }
