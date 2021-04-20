ee67d71b7b09f8c8ba8ebb07e6f2c431c5951700
arturo7123 <>
2021-04-20 04:38:28 +0000

    halfway

--- Midterm/Midterm1.cpp
+++ Midterm/Midterm1.cpp
@@ -2,3 +2,20 @@
 #include <fstream>
 using namespace std;
 std::ifstream infile("students.txt");
+int count = 1;
+int a,b;
+strx;
+double avg;
+int sum;
+int studentcount = 0;
+int main ()
+{
+  fileread(;)
+}
+void fileread(is_void::)
+{
+  ifstream myfile;
+  myfile.open ("students.tt")
+  while(std::getline(infile,line))
+  
+}
