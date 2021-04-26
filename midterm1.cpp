66ea38f0a9425524399c1bc676a8047fdc520670
arturo7123 <>
2021-04-26 03:47:26 +0000

    started over question 1

--- midterm1.cpp
+++ midterm1.cpp
@@ -1,58 +1,16 @@
-#include <iostream>
-#include <fstream>
-using namespace std;
-std::ifstream infile("students.txt");
-int count = 1;
-int a,b;
-str x;
-double avg;
-int sum;
-int studentcount = 0;
-int main ()
+#include<iostream>
+#include<fstream>
+
+using namespace std ;
+
+int main()
 {
-
-  fileread();
-
-}
-
-void fileread(void)
-{
-  ifstream myfile;
-  myfile.open ("students.txt")
-  while(std::getline(infile,line))
-
-{
-  std::istringstream iss(line)
-  if(count%2==0)
-  {
-    while(infile>>a>>b)
-    {
-      summation(a,b);
-      average(a,b);
-
-      cout<<" score 1 : "<< a << " score 2 : "<< b << " sum : " << sum<< "Average : " << avg << end1:
-      if(avg > 80)
-      {
-        studentcount += 1:
-      }
-  }
- }else
- {
-   while(infile>>x)
-  {
-
-    cout<<"student Name : "<< x ;
-  }
- }
-}
-cout<<"The total number of students who has the average > 80 : "<< studentcount<< "Students" << end1;
-myfile.close();
-}
-int summation (int n1, int n2)
-{
-  sum = N1 + N2:
-}
-double average(int n1, int n2)
-{
-  avg = sum/2
-}
+	ifstream std ; 
+	int count = 0 ;
+	std.open("students.txt") ; //opening file 
+	if(std.fail()) //checking file if open or not
+	{
+		cout << "File not working" ;
+	}
+	
+	while(std.good()) //Loop will continue to run until file will be completed 
