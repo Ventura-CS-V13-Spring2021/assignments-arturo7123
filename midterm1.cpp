e40b49dac035c18465198dd108ad2673f18b0ee6
arturo7123 <>
2021-04-26 03:51:34 +0000

    i copied and put down the work the work that i did on off time and re did the entire question

--- midterm1.cpp
+++ midterm1.cpp
@@ -14,3 +14,39 @@ int main()
 	}
 	
 	while(std.good()) //Loop will continue to run until file will be completed 
+  {
+		string name = " " ;   
+		int score_1 = 0 ;
+		int score_2 = 0 ;    // Data variables
+		int sum = 0 ;
+		float avg = 0.0 ;
+
+  		std >> name ;
+		cout << "Student : " << name << "   \t"; // Student name 
+		
+		std >> score_1 ;
+		cout << "Score 1 : " << score_1 << "\t"; // Score 1
+		
+		std >> score_2 ;
+		cout << "Score 2 : " << score_2 << "\t"; // Score 2
+		
+		sum = score_1 + score_2 ;
+		cout << "Sum : " << sum << "\t" ; // Sum 
+		
+		avg = float(sum)/2 ;
+		cout << "Avg : " << avg ; // Average 
+
+    		if(avg>80)
+			count++ ; // Counting students who has average greater then 80 
+			 
+			
+		cout << "\n\n" ; // New line
+		
+	}
+	
+	cout << "\n\n\n<------------------------------------------------------->\n" ;
+	cout << "The total number of students who has average > 80 : " << count << " students.\n\n"; // Print student with average greater the 80
+	
+	std.close(); // Closing file 
+	return 0;
+}
