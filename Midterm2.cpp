b2817d8f266cde973f824d9a60b02230ce7dc854
arturo7123 <>
2021-04-26 03:57:04 +0000

    working on question 2

---
+++ Midterm2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<fstream>
+#include <cstdlib>
+#include <ctime>
+
+//Function prototypes
+int getRdnum();
+int isGreater(int number , int prev_number);
+
+using namespace std;
+int main()
+{
+  int number = 0 ;
+  int n = 10;
+  bool flag = true ;
+}
