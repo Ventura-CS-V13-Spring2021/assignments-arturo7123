9bd59b8a588d8a95e1e1dd8f9394955c9411a7b5
arturo7123 <>
2021-04-26 04:13:32 +0000

    finished and checking code

--- Midterm2.cpp
+++ Midterm2.cpp
@@ -13,4 +13,47 @@ int main()
   int number = 0 ;
   int n = 10;
   bool flag = true ;
+  int prev_num ;
+  ofstream data ;
+
+  data.open("numbers.txt") ; // opens files
+
+  srand(time(NUUll)); // mistake i think
+
+  if(data.fail()) // checks file opening
+  {
+    cout << "File not working!" ;
+  }
+
+  for(int l = 0 ; i<n ; i++) // check later to see if correct
+  {
+    prev_num = number ;
+    number = getRdnum() ; //generates random through Function
+
+
+    if(flag) // checks for first number
+    {
+      flag = false ;
+    }
+    else if(isgreater(number , prev_num)) // checks to see if number is greater maybe miss
+    {
+      data << number << "\t" ;
+    }
+
+    cout << number << "\t";
+
+  }
+
+  data.close() ;
+    return 0 ;
+}
+
+int getRdnum()
+{
+  int num = 0 ;
+
+    for(int i=0,i<10;i++) // loop to generate random numbers
+        num = (rand() % 50) + 1 ;
+  
+  return num ;
 }
