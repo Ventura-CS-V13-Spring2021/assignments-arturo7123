0c831bbddc850fe4f64acf26659b7fb8006d115b
arturo7123 <>
2021-05-11 05:57:18 +0000

    i tested it just to make sure in windows visualizer and it works there but for some reason it is not working in replit

--- quiz7/main.cpp
+++ quiz7/main.cpp
@@ -7,7 +7,8 @@ int main()
 	Coordinate lbval(0.0, 0.0);
 	Coordinate rtval(10.0, 10.0);
 	Rectangle r1(lbval, rtval);
-	r1.printRectangle();
+	
+  r1.printRectangle();
 
 	lbval.setXY(-20.0, -20.0);
 	rtval.setXY(20.0, 20.0);
