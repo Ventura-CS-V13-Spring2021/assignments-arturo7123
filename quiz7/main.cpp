1c074171465d976d0b8453a00eb1083bcc13c4f2
Kyuwoong Lee <>
2021-05-14 00:26:57 +0000

    K

--- quiz7/main.cpp
+++ quiz7/main.cpp
@@ -4,13 +4,13 @@
 int main()
 {
 
-	Coordinate lbval(0.0, 0.0);
+	Coordinate lbval(5.0, 5.0);
 	Coordinate rtval(10.0, 10.0);
 	Rectangle r1(lbval, rtval);
 	
   r1.printRectangle();
 
-	lbval.setXY(-20.0, -20.0);
+	lbval.setXY(0.0, 0.0);
 	rtval.setXY(20.0, 20.0);
 	r1.setLBRT(lbval, rtval);
 	r1.printRectangle();
