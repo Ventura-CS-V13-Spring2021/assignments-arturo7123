9bc3de1ebaaeeb17e32033a67715d5142b507539
arturo7123 <>
2021-05-17 19:13:00 +0000

    started on beginning of question 3

---
+++ Final3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student
+{
+private:
+	string name;
+	int num_classes;
+	string* class_list;
+public:
+	Student() : name(), num_classes(0), class_list(NULL) {};
+	Student(string n, int num)
+	{
+		name = n;
+		num_classes = num;
+		class_list = new string[num];
+		for (int i = 0; i < num; i++)
+		{
+			cout << "Enter the course name : ";
+			cin >> class_list[i];
+		}
+	}
+	Student(Student& rhs)
+	{
+		name = rhs.name;
+		num_classes = rhs.num_classes;
+		class_list = new string[num_classes];
+		for (int i = 0; i < rhs.num_classes; i++)
+			class_list[i] = rhs.class_list[i];
