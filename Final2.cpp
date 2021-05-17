355fb2fad6dae714e53cf96979e3d708d45a80c8
arturo7123 <>
2021-05-17 18:11:34 +0000

    fixed a couple things that were causing errors like spelling mistakes and typing in a - instead of an =

--- Final2.cpp
+++ Final2.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 void sort(int arr[], int sum
 {
-  for (int i = 0; i< nu-1; i++)
-    for (int j = i; j<num; j++)
-      if(arr[i]< arr [j])
-        swao(arr[i], arr[j])
+  for (int i = 0; i < num - 1; i++)
+    for (int j = i; j < num; j++)
+      if (arr[i] < arr [j])
+        swap(arr[i], arr[j]);
 }
 int main()
 {
   int arr[20];
-  int num = 2-, count;
-  cout<< "enter the elemnts of the array ( -999 to terminate input): ";
+  int num = 20, count;
+  cout<< "Enter the elemnts of the array ( -999 to terminate input): ";
   for (int i = 0; i < num; i++)
-  }
+  {
   cin >> arr[i];
   if (arr[i] == -999)
     {
@@ -22,16 +22,17 @@ int main()
       break;
     }
   }
-  sort( arr, num);
-  cout << " N count\n";
-  for ( int i = 0; i< num i++)
+  sort(arr, num);
+  cout << " N Count\n";
+  for (int i = 0; i < num; i++)
   {
-    cout << setw(3) << right < arr[i];
+    cout << setw(3) << right << arr[i];
     count = 0;
-    for ( int j - i; arr[i] == arr [j]; j++)
+    for (int j = i; arr[i] == arr [j]; j++)
     {
       count++;
     }
     cout << setw(4) << right << count << end1;
     i += (count - 1);
   }
+}
