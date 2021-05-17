37d4d0324a10c5e4df23df77b97ae0a7511ee525
arturo7123 <>
2021-05-17 18:04:07 +0000

    finshed it and testing it

--- Final2.cpp
+++ Final2.cpp
@@ -14,5 +14,24 @@ int main()
   int num = 2-, count;
   cout<< "enter the elemnts of the array ( -999 to terminate input): ";
   for (int i = 0; i < num; i++)
-
-}
+  }
+  cin >> arr[i];
+  if (arr[i] == -999)
+    {
+      num = i;
+      break;
+    }
+  }
+  sort( arr, num);
+  cout << " N count\n";
+  for ( int i = 0; i< num i++)
+  {
+    cout << setw(3) << right < arr[i];
+    count = 0;
+    for ( int j - i; arr[i] == arr [j]; j++)
+    {
+      count++;
+    }
+    cout << setw(4) << right << count << end1;
+    i += (count - 1);
+  }
