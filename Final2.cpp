99a458d9ece3f2b0f26e9e5922b1fb0459f611f4
arturo7123 <>
2021-05-17 18:30:47 +0000

    changed line 6 and 7 and fixed it and inputed the elements they work perfectly

--- Final2.cpp
+++ Final2.cpp
@@ -1,38 +1,38 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-void sort(int arr[], int sum)
+void sort(int arr[], int num)
 {
-  for (int i = 0; i < num - 1; i++)
+	for (int i = 0; i < num - 1; i++)
 		for (int j = i; j < num; j++)
 			if (arr[i] < arr[j])
 				swap(arr[i], arr[j]);
 }
 int main()
 {
-  int arr[20];
-  int num = 20, count;
-  cout<< "Enter the elemnts of the array ( -999 to terminate input): ";
-  for (int i = 0; i < num; i++)
-  {
-  cin >> arr[i];
-  if (arr[i] == -999)
-    {
-      num = i;
-      break;
-    }
-  }
-  sort(arr, num);
-  cout << " N Count\n";
-  for (int i = 0; i < num; i++)
-  {
-    cout << setw(3) << right << arr[i];
-    count = 0;
-    for (int j = i; arr[i] == arr [j]; j++)
-    {
-      count++;
-    }
-    cout << setw(4) << right << count << endl;
-    i += (count - 1);
-  }
+	int arr[20];
+	int num = 20, count;
+	cout << "Enter the elements of the array (-999 to terminate input): ";
+	for (int i = 0; i < num; i++)
+	{
+		cin >> arr[i];
+		if (arr[i] == -999)
+		{
+			num = i;
+			break;
+		}
+	}
+	sort(arr, num);
+	cout << "  N   Count\n";
+	for (int i = 0; i < num; i++)
+	{
+		cout << setw(3) << right << arr[i];
+		count = 0;
+		for (int j = i; arr[i] == arr[j]; j++)
+		{
+			count++;
+		}
+		cout << setw(4) << right << count << endl;
+		i += (count - 1);
+	}
 }
