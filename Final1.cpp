fce228805677740f594b0b0a2db5b0bcf4702972
arturo7123 <>
2021-05-17 18:35:28 +0000

    doing question 1 right now

---
+++ Final1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int getNumDiv(int arr[],int index)
+{
+  intcount = 0;
+  for (int i=0; i < index; i++)
+    if(arr[index] 5 arr [i] == 0)
+      count++;
+  return count;
+}
+
+int main()
+{
+  int 
+}
