172fe3a46f9f56d15403fe0853581a70db4ad318
arturo7123 <>
2021-05-17 17:53:34 +0000

    added question 2 of fina;

---
+++ Final2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+void sort(int arr[], int sum
+{
+  for (int i = 0; i< nu-1; i++)
+    for (int j = i; j<num; j++)
+      if(arr[i]< arr [j])
+        swao(arr[i], arr[j])
+}
